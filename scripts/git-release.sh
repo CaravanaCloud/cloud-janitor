b1526b7a646bdb8450ab8971c36e1c0be46aba0f
Julio Faerman <jfaerman@gmail.com>
2022-03-02 14:52:07 +0100

    Initial github action

--- scripts/git-release.sh
+++ scripts/git-release.sh
@@ -3,4 +3,5 @@
 DATESTAMP=$(date +%Y%m%d-%H%M%S)
 TAG="tasktree-$DATESTAMP"
 NOTES=${NOTES:-"Test release notes."}
-gh release create "$TAG" --notes "$NOTES"
+git tag $TAG -m "$NOTES"
+git push origin $TAG
