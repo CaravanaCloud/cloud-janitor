bcf5a1b3d38d68693cfa5ba1d99716759d8949ed
Julio Faerman <jfaerman@gmail.com>
2022-03-05 11:27:48 +0100

    at 20220305-112748

---
+++ docs/setup_flow.md
@@ -0,0 +1,9 @@
+# Today we'll build
+
+- Microservices style application
+- Secure from the start
+
+Featuring:
+- Quarkus
+- Keycloak
+
--- scripts/git-release.sh
+++ scripts/git-release.sh
@@ -1,7 +1,10 @@
 #/bin/bash
 
-DATESTAMP=$(date +%Y%m%d-%H%M%S)
-TAG="tasktree-$DATESTAMP"
-NOTES=${NOTES:-"Test release notes."}
+MAJOR=0
+MINOR=1
+PATCH=$(date +%Y%m%d%H%M%S)
+VERSION="$MAJOR.$MINOR.$PATCH"
+TAG="tasktree-$VERSION"
+NOTES=${NOTES:-"TaskTree release $VERSION"}
 git tag $TAG -m "$NOTES"
 git push origin $TAG
