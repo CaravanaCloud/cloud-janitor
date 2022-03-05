b759958748e6aa90d619dccbee9c1c2e7e2d2952
Julio Faerman <jfaerman@gmail.com>
2022-03-05 10:41:01 +0100

    at 20220305-104058

---
+++ scripts/build-all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RDIR="$DIR/.."
+
+mvn clean package -Dquarkus.package.type=uber-jar
+
+jpackage --input "$RDIR/tt-cli/target" \
+  --main-jar "tasktree-runner.jar" \
+  --name "tasktree" \
+  --main-class "tasktree.Main" \
+  --type "rpm" \
+  --java-options "--enable-preview" \
+  --app-version "0.0.1" \
+  --description "TaskTree RPM Package" \
+  --vendor "CaravanaCloud" \
+#  --verbose
+
+echo "done"
