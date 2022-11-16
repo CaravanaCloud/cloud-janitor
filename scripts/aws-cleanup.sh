c4ec8e4c5acbe49a9f16254d66446cf03a08f969
Julio Faerman <jufaerma@redhat.com>
2022-11-16 17:50:53 +0100

    Lint me up

--- scripts/aws-cleanup.sh
+++ scripts/aws-cleanup.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PDIR="$(dirname "$DIR")"
 
-$PDIR/mvnw clean compile exec:java \
+"${PDIR}/mvnw" clean compile exec:java \
   -Dexec.mainClass='tasktree.Main' \
   -Dquarkus.profile=awscleanup
