23d2fd676fce8201cb2200d998bbce7a6147e24f
Julio Faerman <jufaerma@redhat.com>
2022-11-18 19:11:04 +0100

    Improved arguments parsing Configurable log level

--- src/main/java/cj/CommandJanitor.java
+++ src/main/java/cj/CommandJanitor.java
@@ -1,66 +1,79 @@
 package cj;
 
+import cj.fs.FSUtils;
+import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
-import io.quarkus.runtime.annotations.StaticInitSafe;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
 import javax.inject.Inject;
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.Callable;
 
 @CommandLine.Command(version = "1.3.6",
         mixinStandardHelpOptions = true,
         name = "cloud-janitor", description = "Cloud Janitor at your service.")
-@StaticInitSafe
-public class CommandJanitor implements Runnable,
-        QuarkusApplication {
-    @Inject
-    CommandLine.IFactory factory;
-
+public class CommandJanitor implements Callable<Integer>, QuarkusApplication {
+    private static final Logger log = LoggerFactory.getLogger(CommandJanitor.class);
     @Inject
     CloudJanitor cj;
-    @Inject
-    Tasks tasks;
 
-    @Inject
-    Logger log;
 
     @SuppressWarnings("all")
     @CommandLine.Option(names = {"-t", "--task"}, description = "Task to be executed. Try '-t hello'")
-    Optional<String> taskName;
+    String taskName;
 
     @CommandLine.Option(names = {"-i", "--input"}, description = "Input parameters for the task, repeatable.")
     List<String> input;
 
     @CommandLine.Option(names = {"-c", "--capabilities"}, description = "Feature toggles (try -c 'all').")
-    List<String> capabilities;
+    String capabilities;
+
+    @CommandLine.Option(names = {"-l", "--log-level"}, description = "log level (try -l 'debug').")
+    String logLevel;
 
     public CommandJanitor(){}
 
     @Override
-    public void run() {
-        log.trace("Command.run()");
+    public Integer call() throws Exception {
         parseArgs();
-        cj.run();
+        Quarkus.run(CommandJanitor.class);
+        return 0;
     }
 
     private void parseArgs() {
-        taskName.ifPresent(tasks::setTask);
-        if (input != null)
-            input.forEach(tasks::addInput);
-        if (capabilities != null)            
-            capabilities.forEach(tasks::addCapability);
-        if (input != null)
-            input.forEach(tasks::addInput);
+        trySetProperty("quarkus.log.console.level", logLevel);
+        trySetProperty("cj.task", taskName);
+        trySetProperty("cj.capabilities", capabilities);
+        if (input != null) for(var i:input){
+            trySetProperty(i, i);
+        }
+        loadLocalQuarkusConfig();
+    }
+
+    private void trySetProperty(String systemProperty, String argument) {
+        if(argument != null){
+            log.debug("{} := {}", systemProperty, argument);
+            System.setProperty(systemProperty, argument);
+        }
     }
 
     @Override
     public int run(String... args) throws Exception {
         log.trace("Command.run(...)");
-        @SuppressWarnings("redundant")
-        var exit = new CommandLine(this, factory).execute(args);
-        return exit;
+        cj.run();
+        return 0;
+    }
+
+    private static void loadLocalQuarkusConfig() {
+        var localConfigDir = FSUtils.getLocalConfigDir();
+        var localConfigFile = localConfigDir.resolve("application.yaml");
+        if (localConfigFile.toFile().exists()){
+            var configLocation = localConfigDir.toAbsolutePath().toString();
+            //System.out.println("Local configuration file found at " + configLocation);
+            System.setProperty("quarkus.config.locations", configLocation);
+        }
     }
 
 }
--- src/main/java/cj/Main.java
+++ src/main/java/cj/Main.java
@@ -1,26 +1,15 @@
 package cj;
 
-import cj.fs.FSUtils;
-import io.quarkus.runtime.Quarkus;
+
 import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.runtime.annotations.StaticInitSafe;
+import picocli.CommandLine;
 
 @QuarkusMain
+@StaticInitSafe
 public class Main {
-
     public static void main(String[] args) {
-        loadLocalQuarkusConfig();
-        Quarkus.run(CommandJanitor.class, args);
-    }
-
-    private static void loadLocalQuarkusConfig() {
-        var localConfigDir = FSUtils.getLocalConfigDir();
-        var localConfigFile = localConfigDir.resolve("application.yaml");
-        if (localConfigFile.toFile().exists()){
-            var configLocation = localConfigDir.toAbsolutePath().toString();
-            //System.out.println("Local configuration file found at " + configLocation);
-            System.setProperty("quarkus.config.locations", configLocation);
-        }else {
-            //System.err.println("Local configuration file not found.");
-        }
+        int exitCode = new CommandLine(new CommandJanitor()).execute(args);
+        System.exit(exitCode);
     }
 }
