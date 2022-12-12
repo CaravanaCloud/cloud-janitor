c26941253449ce74e2847ca52f049a64ab381cbe
Julio Faerman <jufaerma@redhat.com>
2022-12-12 12:46:43 +0100

    1.7.1 AWS UG SP release

--- src/main/java/cj/CloudJanitor.java
+++ src/main/java/cj/CloudJanitor.java
@@ -13,7 +13,7 @@ import javax.inject.Inject;
 
 @ApplicationScoped
 public class CloudJanitor implements QuarkusApplication {
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.7.2";
     @Inject
     Logger log;
 
